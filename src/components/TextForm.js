8803eb688f885d82ad4751dba7d3f3513269e6df
halaironik <ronikhalai56@gmail.com>
2022-08-24 16:50:24 -0400

    Removed some unnecessary comments

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,26 +1,20 @@
 import React, {useState} from 'react'
 
-
 export default function TextForm(props) {
     const handleUpClick = ()=>{
-        // console.log("Uppercase was clicked: " +  text);
         let newText = text.toUpperCase();
         setText(newText)
     }
     const handleLoClick = ()=>{
-        // console.log("Uppercase was clicked: " +  text);
         let newText = text.toLowerCase();
         setText(newText)
     }
 
     const handleOnChange = (event)=>{
-        // console.log("On change");
         setText(event.target.value)
     }
 
     const [text, setText] = useState(''); 
-    // text = "new text"; // Wrong way to change the state
-    // setText("new text"); // Correct way to change the state
     return (
         <>
         <div className="container"> 
